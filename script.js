0e8e2202e3393c58f96dd7c862d9bd831a8a501a
carmenmontalvo737-pixel <carmenmontalvo737@gmail.com>
2026-05-31 11:18:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Función de bienvenida
+function mostrarMensaje() {
+    alert("Bienvenido al Sistema de Inventario Creativo");
+}
+
+// Comentario:
+// Este archivo JavaScript contiene funciones básicas
+// para interacción del usuario.
